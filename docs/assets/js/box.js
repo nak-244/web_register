02f49139cd94ee7af7189fa5600d95bf4c88565b
nak-244 <t09024656860@gmail.com>
2020-01-29 18:54:38 +0900

    up

---
+++ docs/assets/js/box.js
@@ -0,0 +1,197 @@
+$(function() {
+$('#srh_ken_param').change(function() {
+  var val = $(this).val();
+  switch (val) {
+    case '1':
+    $('#box1').show();
+    $('div[id^="box"]:not("#box1")').hide();
+    break;
+    case '2':
+    $('#box2').show();
+    $('div[id^="box"]:not("#box2")').hide();
+    break;
+    case '3':
+    $('#box3').show();
+    $('div[id^="box"]:not("#box3")').hide();
+    break;
+    case '4':
+    $('#box4').show();
+    $('div[id^="box"]:not("#box4")').hide();
+    break;
+    case '5':
+    $('#box5').show();
+    $('div[id^="box"]:not("#box5")').hide();
+    break;
+    case '6':
+    $('#box6').show();
+    $('div[id^="box"]:not("#box6")').hide();
+    break;
+    case '7':
+    $('#box7').show();
+    $('div[id^="box"]:not("#box7")').hide();
+    break;
+    case '8':
+    $('#box8').show();
+    $('div[id^="box"]:not("#box8")').hide();
+    break;
+    case '9':
+    $('#box9').show();
+    $('div[id^="box"]:not("#box9")').hide();
+    break;
+    case '10':
+    $('#box10').show();
+    $('div[id^="box"]:not("#box10")').hide();
+    break;
+    case '11':
+    $('#box11').show();
+    $('div[id^="box"]:not("#box11")').hide();
+    break;
+    case '12':
+    $('#box12').show();
+    $('div[id^="box"]:not("#box12")').hide();
+    break;
+    case '13':
+    $('#box13').show();
+    $('div[id^="box"]:not("#box13")').hide();
+    break;
+    case '14':
+    $('#box14').show();
+    $('div[id^="box"]:not("#box14")').hide();
+    break;
+    case '15':
+    $('#box15').show();
+    $('div[id^="box"]:not("#box15")').hide();
+    break;
+    case '16':
+    $('#box16').show();
+    $('div[id^="box"]:not("#box16")').hide();
+    break;
+    case '17':
+    $('#box17').show();
+    $('div[id^="box"]:not("#box17")').hide();
+    break;
+    case '18':
+    $('#box18').show();
+    $('div[id^="box"]:not("#box18")').hide();
+    break;
+    case '19':
+    $('#box19').show();
+    $('div[id^="box"]:not("#box19")').hide();
+    break;
+    case '20':
+    $('#box20').show();
+    $('div[id^="box"]:not("#box20")').hide();
+    break;
+    case '21':
+    $('#box21').show();
+    $('div[id^="box"]:not("#box21")').hide();
+    break;
+    case '22':
+    $('#box22').show();
+    $('div[id^="box"]:not("#box22")').hide();
+    break;
+    case '23':
+    $('#box23').show();
+    $('div[id^="box"]:not("#box23")').hide();
+    break;
+    case '24':
+    $('#box24').show();
+    $('div[id^="box"]:not("#box24")').hide();
+    break;
+    case '25':
+    $('#box25').show();
+    $('div[id^="box"]:not("#box25")').hide();
+    break;
+    case '26':
+    $('#box26').show();
+    $('div[id^="box"]:not("#box26")').hide();
+    break;
+    case '27':
+    $('#box27').show();
+    $('div[id^="box"]:not("#box27")').hide();
+    break;
+    case '28':
+    $('#box28').show();
+    $('div[id^="box"]:not("#box28")').hide();
+    break;
+    case '29':
+    $('#box29').show();
+    $('div[id^="box"]:not("#box29")').hide();
+    break;
+    case '30':
+    $('#box30').show();
+    $('div[id^="box"]:not("#box30")').hide();
+    break;
+    case '31':
+    $('#box31').show();
+    $('div[id^="box"]:not("#box31")').hide();
+    break;
+    case '32':
+    $('#box32').show();
+    $('div[id^="box"]:not("#box32")').hide();
+    break;
+    case '33':
+    $('#box33').show();
+    $('div[id^="box"]:not("#box33)').hide();
+    break;
+    case '34':
+    $('#box34').show();
+    $('div[id^="box"]:not("#box34")').hide();
+    break;
+    case '35':
+    $('#box35').show();
+    $('div[id^="box"]:not("#box35")').hide();
+    break;
+    case '36':
+    $('#box36').show();
+    $('div[id^="box"]:not("#box36")').hide();
+    break;
+    case '37':
+    $('#box37').show();
+    $('div[id^="box"]:not("#box37")').hide();
+    break;
+    case '38':
+    $('#box38').show();
+    $('div[id^="box"]:not("#box38")').hide();
+    break;
+    case '39':
+    $('#box39').show();
+    $('div[id^="box"]:not("#box39")').hide();
+    break;
+    case '40':
+    $('#box40').show();
+    $('div[id^="box"]:not("#box40")').hide();
+    break;
+    case '41':
+    $('#box41').show();
+    $('div[id^="box"]:not("#box41")').hide();
+    break;
+    case '42':
+    $('#box42').show();
+    $('div[id^="box"]:not("#box42")').hide();
+    break;
+    case '43':
+    $('#box43').show();
+    $('div[id^="box"]:not("#box43")').hide();
+    break;
+    case '44':
+    $('#box44').show();
+    $('div[id^="box"]:not("#box44")').hide();
+    break;
+    case '45':
+    $('#box45').show();
+    $('div[id^="box"]:not("#box45")').hide();
+    break;
+    case '46':
+    $('#box46').show();
+    $('div[id^="box"]:not("#box46")').hide();
+    break;
+    case '47':
+    $('#box47').show();
+    $('div[id^="box"]:not("#box47")').hide();
+    break;
+    default:
+    $('div[id^="box"]').hide();
+  }
+});
+});
