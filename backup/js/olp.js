82492ae24496684516b95f4a35ceda1b1ad87326
nak-244 <t09024656860@gmail.com>
2020-01-30 14:52:18 +0900

    up

--- backup/js/olp.js
+++ backup/js/olp.js
@@ -32,81 +32,23 @@ $(function() {
   });
 });
 
-// 名前　必須項目入力しないと次へボタンがクリックできない
-$(function() {
-  //始めにjQueryでボタンを無効化する
-  $('.send1').prop("disabled", true);
-
-  //入力欄の操作時
-  $('form div#name input:required').change(function() {
-    //必須項目が空かどうかフラグ
-    let flag = true;
-    //必須項目をひとつずつチェック
-    $('form div#name input:required').each(function(e) {
-      //もし必須項目が空なら
-      if ($('form div#name input:required').eq(e).val() === "") {
-        flag = false;
-      }
+// modal固定用
+$(function(){
+    var scrollPosition;
+    $(".modalOpner").on("click", function() {
+        scrollPosition = $(window).scrollTop();
+        $('body').addClass('fixed').css({'top': -scrollPosition});
     });
-    //全て埋まっていたら
-    if (flag) {
-      //送信ボタンを復活
-      $('.send1').prop("disabled", false);
-    } else {
-      //送信ボタンを閉じる
-      $('.send1').prop("disabled", true);
-    }
-  });
-});
-
-// 性別　どちらか選ばないと次へボタンがクリックできない
-$(function() {
-  //始めにjQueryでボタンを無効化する
-  $('#send2').prop("disabled", true);
-  //入力欄の操作時
-  $(".send2").click(function() {
-    //ラジオボタンのどちらかがチェックされたら
-    if (this.checked) {
-      //ボタンを復活
-      $('#send2').prop("disabled", false);
-    } else {
-      //選択されるまではボタンを無効化
-      $('#send2').prop("disabled", true);
-    };
-  });
-});
-
-// 生年月日　すべて選択しないと次へボタンがクリックできない
-$(function() {
-  //始めにjQueryでボタンを無効化する
-  $('.send3').prop("disabled", true);
-
-  //入力欄の操作時
-  $('form div#birth input:required').change(function() {
-    //必須項目が空かどうかフラグ
-    let flag = true;
-    //必須項目をひとつずつチェック
-    $('form div#birth input:required').each(function(e) {
-      //もし必須項目が空なら
-      if ($('form div#birth input:required').eq(e).val() === "") {
-        flag = false;
-      }
+    $(".modalCloser").on("click", function() {
+        $('body').removeClass('fixed').css({'top': 0});
+        window.scrollTo( 0 , scrollPosition );
     });
-    //全て埋まっていたら
-    if (flag) {
-      //送信ボタンを復活
-      $('.send3').prop("disabled", false);
-    } else {
-      //送信ボタンを閉じる
-      $('.send3').prop("disabled", true);
-    }
-  });
 });
 
 // メールアドレス　入力しないと次へボタンがクリックできない
 $(function() {
   //始めにjQueryでボタンを無効化する
-  $('.send4').prop("disabled", true);
+  $('.send1').prop("disabled", true);
 
   //入力欄の操作時
   $('form div#mail input:required').change(function() {
@@ -122,62 +64,10 @@ $(function() {
     //全て埋まっていたら
     if (flag) {
       //送信ボタンを復活
-      $('.send4').prop("disabled", false);
+      $('.send1').prop("disabled", false);
     } else {
       //送信ボタンを閉じる
-      $('.send4').prop("disabled", true);
+      $('.send1').prop("disabled", true);
     }
-  });
-});
-
-// 住所　郵便番号を入力しないと次へボタンがクリックできない
-$(function() {
-  //始めにjQueryでボタンを無効化する
-  $('.send5').prop("disabled", true);
-
-  //入力欄の操作時
-  $('form div#address input:required').change(function() {
-    //必須項目が空かどうかフラグ
-    let flag = true;
-    //必須項目をひとつずつチェック
-    $('form div#address input:required').each(function(e) {
-      //もし必須項目が空なら
-      if ($('form div#address input:required').eq(e).val() === "") {
-        flag = false;
-      }
-    });
-    //全て埋まっていたら
-    if (flag) {
-      //送信ボタンを復活
-      $('.send5').prop("disabled", false);
-    } else {
-      //送信ボタンを閉じる
-      $('.send5').prop("disabled", true);
-    }
-  });
-});
-
-// 希望職種　1つ以上選択しないと送信ボタンがクリックできない
-$(function() {
-  //始めにjQueryでボタンを無効化する
-  $('.send6').prop("disabled", true);
-
-  $(".jobjob").click(function() {
-    var ck_count = $(".cb :checked").length;
-    if (ck_count == 0) {
-      $('.send6').prop("disabled", false);
-    } else {
-      $('.send6').prop("disabled", true);
-    }
-  });
-});
-
-// 自動でフリガナをふる
-$(function() {
-  $.fn.autoKana('#name1', '#namekana1', {
-    katakana: true
-  });
-  $.fn.autoKana('#name2', '#namekana2', {
-    katakana: true
   });
 });
